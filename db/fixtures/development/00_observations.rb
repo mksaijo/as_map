2f26f058c9fe49e50f01487d47952e31a4b18cb0
mksaijo <saijo.0830@gmail.com>
2023-11-02 12:03:47 +0900

    seed_fuでデータを流し込む

---
+++ db/fixtures/development/00_observations.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+file_path = Rails.root.join("db/fixtures/development/observations.csv")
+CSV.read(file_path, headers: true).each do |row|
+  Observation.seed(:id) do |s|
+    s.date = row[0]
+    s.count = row[1]
+    s.long = row[2]
+    s.lat = row[3]
+  end
+end
