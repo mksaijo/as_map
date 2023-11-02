c49921e6d12f800dd81a279ca4d63359c756b3aa
mksaijo <saijo.0830@gmail.com>
2023-11-02 11:25:14 +0900

    スキーマの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_015922) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_02_021830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "observations", force: :cascade do |t|
     t.date "date"
     t.integer "count"
-    t.integer "Loc"
+    t.integer "Long"
     t.integer "Lat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
