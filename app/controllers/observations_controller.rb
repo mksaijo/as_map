dba8ae40f0c4f7be47b6752add1fe3edc6b0136e
mksaijo <saijo.0830@gmail.com>
2023-11-02 15:22:26 +0900

    geojson形式でAPI書き出し

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,11 +1,26 @@
 class ObservationsController < ApplicationController
   def index
-    obs = Observation.order(date: :desc)
-    render json: { status: 'SUCCESS', message: 'Loaded observations', data: obs }
+    observations = Observation.order(date: :desc)
+    features = observations.map do |obs|
+      RGeo::GeoJSON::Feature.new(
+        RGeo::Geographic.spherical_factory.point(obs.long, obs.lat),
+        obs.id,
+        count: obs.count,
+        date: obs.date
+      )
+    end
+    feature_collection = RGeo::GeoJSON::FeatureCollection.new(features)
+    render json: RGeo::GeoJSON.encode(feature_collection)  
   end
 
   def show
     ob = Observation.find(params[:id])
-    render json: { status: 'SUCCESS', message: 'Loaded observation', data: ob }
+    feature = RGeo::GeoJSON::Feature.new(
+      RGeo::Geographic.spherical_factory.point(ob.long, ob.lat),
+      ob.id,
+      count: ob.count,
+      date: ob.date
+    )
+    render json: RGeo::GeoJSON.encode(feature)
   end
 end
