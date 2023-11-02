70f4aaf342b0494a16e5303e7b9c358f47f682b7
mksaijo <saijo.0830@gmail.com>
2023-11-02 13:29:08 +0900

    show

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -3,4 +3,9 @@ class ObservationsController < ApplicationController
     obs = Observation.order(date: :desc)
     render json: { status: 'SUCCESS', message: 'Loaded posts', data: obs }
   end
+
+  def show
+    ob = Observation.find(params[:id])
+    render json: { status: 'SUCCESS', message: 'Loaded posts', data: ob }
+  end
 end
