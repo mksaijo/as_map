8ea666fa56d8381fdc243805fd05de44856661b0
mksaijo <saijo.0830@gmail.com>
2023-11-02 13:26:51 +0900

    修正

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,11 +1,6 @@
-module Api
-  module V1
-    class ObservationsController < ApplicationController
-      def index
-        obs = Observation.order(date: :desc)
-        render json: { status: 'SUCCESS', message: 'Loaded posts', data: obs }
-      end
-
-    end
+class ObservationsController < ApplicationController
+  def index
+    obs = Observation.order(date: :desc)
+    render json: { status: 'SUCCESS', message: 'Loaded posts', data: obs }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
-      resources :observations
-    end
-  end
+  resources :observations
 end
